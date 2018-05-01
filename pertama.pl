d0a2616122856fa3a4a24ae7d7426647722a7195
AnandaRauf <uzumaki.nanda@rocketmail.com>
2018-05-01 12:32:33 +0700

    versi 1.0.0

---
+++ pertama.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+# contoh program Selamat Anda Sukses Belajar Code Pemrogaman Perl
+# ini adalah baris komentar
+# nama program : perl pertama.pl 
+# Author : Ananda Rauf Maududi™ 30 April ©2018
+print “Selamat Anda Sukses Belajar Code Pemrogaman Perl”; $loop;
+print “Selamat Datang Di Code Pemrogaman Perl dan Semoga Sukses menjadi IT Senior”; $loop;
